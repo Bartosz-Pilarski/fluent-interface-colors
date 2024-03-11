31f52cf0eb7120e1e9647cda162d78d150113fc1
Bartosz-Pilarski <bpilarski2003@protonmail.com>
2024-03-11 13:23:23 +0100

    Legacy compatibility

--- src/colors.js
+++ src/colors.js
@@ -6,36 +6,31 @@ export default function() {
   //Message currently styled
   let stylePrefix = ''
 
-  const colors = {
-    //Finalize all messages and render to console
-    log: (newMessage) => {
-      console.log(`${message}${stylePrefix}${newMessage}\x1b[0m`)
-      stylePrefix = ''
-      message = ''
-      return colors
-    },
-    //Lock in current message and styling, and reset styles for the next message.
-    then: (newMessage) => {
-      message += `${stylePrefix}${newMessage}\x1b[0m`
-      stylePrefix = ''
-      return colors
-    },
-    //Lock in current message and styling without resetting styles
-    and: (newMessage) => {
-      message += `${stylePrefix}${newMessage}`
-      return colors
-    }
-  }
-
   //Define color functions dynamically, based on array elements
   Object.keys(ansi).forEach((key) => {
-    Object.defineProperty(colors, key, {
-      get: () => { 
-        stylePrefix += ansi[key]
-        return colors
-      }
-    })
+    this[key] = () => {
+      stylePrefix += ansi[key]
+      return this
+    }
   })
 
-  return colors
+  //Lock in current message and styling, and reset styles for the next message.
+  this.then = (newMessage) => {
+    message += `${stylePrefix}${newMessage}\x1b[0m`
+    stylePrefix = ''
+    return this
+  }
+  //Lock in current message and styling without resetting styles
+  this.and = (newMessage) => {
+    message += `${stylePrefix}${newMessage}`
+    return this
+  }
+  
+  //Finalize all messages and render to console
+  this.log = (newMessage) => {
+    console.log(`${message}${stylePrefix}${newMessage}\x1b[0m`)
+    stylePrefix = ''
+    message = ''
+    return this
+  }
 }
